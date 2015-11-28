12c16d994d9323c5db8b3a5af909b2d4e30f6f44
tennysondy <tennyson_dy@sina.com>
2015-11-28 19:18:10 +0800

    增加sskeychain，reachability

--- WalkShopper/AppConfiguration/WSAppGeneralConfiguration.m
+++ WalkShopper/AppConfiguration/WSAppGeneralConfiguration.m
@@ -39,7 +39,54 @@
 
 - (NSString *)uniqueID
 {
+    
     return [[[ASIdentifierManager sharedManager] advertisingIdentifier] UUIDString];
 }
+
+- (NSString *)appleIFA
+{
+    NSString *ifa = nil;
+    Class ASIdentifierManagerClass = NSClassFromString(@"ASIdentifierManager");
+    // a dynamic way of checking if AdSupport.framework is available
+    if (ASIdentifierManagerClass) {
+        ifa = [[[ASIdentifierManager sharedManager] advertisingIdentifier] UUIDString];
+    }
+    return ifa;
+}
+
+- (NSString *)appleIFV
+{
+    if(NSClassFromString(@"UIDevice") && [UIDevice instancesRespondToSelector:@selector(identifierForVendor)]) {
+        // only available in iOS >= 6.0
+        return [[UIDevice currentDevice].identifierForVendor UUIDString];
+    }
+    return nil;
+}
+
+- (NSString *)randomUUID
+{
+    // only available in iOS >= 6.0
+    if(NSClassFromString(@"NSUUID")) {
+        return [[NSUUID UUID] UUIDString];
+    }
+    CFUUIDRef uuidRef = CFUUIDCreate(kCFAllocatorDefault);
+    CFStringRef cfuuid = CFUUIDCreateString(kCFAllocatorDefault, uuidRef);
+    CFRelease(uuidRef);
+    NSString *uuid = [((__bridge NSString *) cfuuid) copy];
+    CFRelease(cfuuid);
+    return uuid;
+}
+
++ (void)setValue:(NSString *)value forKey:(NSString *)key inService:(NSString *)service
+{
+    NSMutableDictionary *keychainItem = [[NSMutableDictionary alloc] init];
+    keychainItem[(__bridge id)kSecClass] = (__bridge id)kSecClassGenericPassword;
+    keychainItem[(__bridge id)kSecAttrAccessible] = (__bridge id)kSecAttrAccessibleAlways;
+    keychainItem[(__bridge id)kSecAttrAccount] = key;
+    keychainItem[(__bridge id)kSecAttrService] = service;
+    keychainItem[(__bridge id)kSecValueData] = [value dataUsingEncoding:NSUTF8StringEncoding];
+    SecItemAdd((__bridge CFDictionaryRef)keychainItem, NULL);
+}
+
 
 @end
